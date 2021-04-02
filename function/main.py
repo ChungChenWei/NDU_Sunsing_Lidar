6e95175fe9f2917571bbee9b3918fb93777a1a35
jellyyrr <s1000210hank@icloud.com>
2021-04-02 14:56:00 +0800

    20210402 : change main.py

--- function/main.py
+++ function/main.py
@@ -3,38 +3,28 @@
 
 # new	  	: 
 # structure : 
-# target  	: s
+# target  	: 
 # 1. 
 import sys
 from datetime import datetime as dtm
 from datetime import timedelta as dtmdt
 from os.path import join as pth
-
-if __name__=='__main__':
-	print(f'\nprogram : {sys.argv[0]}')
-	print('version : \n')
-
-	start = dtm.now().timestamp()
-	print(f"program start : {dtm.now().strftime('%m/%d %X')}\n")
-
-  final = dtm.now().timestamp()
-  runtime = (final-start)/60.
-  print(f'\nProgram done\nrunning time = {int(runtime):3d} min {(runtime-int(runtime))*60.:6.3f} s')
+from lidar.dt_handle import *
+from func_dt_handle import * 
     
 
 def run():
-    # import numpy as n
 
-    # from func_dt_handle import * 
+    
     # from func_plot import *
 
-    from lidar.dt_handle import *
+    
 
-    # start_dtm = dtm(2020,11,20,0,0,0)
-    # final_dtm = dtm(2020,11,21,0,0,0)
-    # path = pth('..','data','Lidar_Sunsing_NDU_testdata')
-    # reader = NDU.reader(path,start_dtm,final_dtm,reset=False)
-    # dt = reader.get_data()
+    start_dtm = dtm(2020,11,20,0,0,0)
+    final_dtm = dtm(2020,11,21,0,0,0)
+    path = pth('..','data','Lidar_Sunsing_NDU_testdata')
+    reader = NDU.reader(path,start_dtm,final_dtm,reset=False)
+    dt = reader.get_data()
 
     # start_dtm = dtm(2015,1,23,12,30,0)
     # final_dtm = dtm(2015,1,23,15,30,0)
@@ -53,3 +43,23 @@ def run():
     # path = pth('..','data','Lidar_Sunsing_TORI_testdata')
     # reader = lidar_TORI(path,start_dtm,final_dtm,reset=True)
     # dt3 = reader.get_data()
+
+
+
+
+
+if __name__=='__main__':
+	print(f'\nprogram : {sys.argv[0]}')
+	print('version : \n')
+	start = dtm.now().timestamp()
+	print(f"program start : {dtm.now().strftime('%m/%d %X')}\n")
+
+
+	run()
+
+
+	final = dtm.now().timestamp()
+	runtime = (final-start)/60.
+	print(f'\nProgram done\nrunning time = {int(runtime):3d} min {(runtime-int(runtime))*60.:6.3f} s')
+
+
